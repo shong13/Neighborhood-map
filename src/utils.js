3610cd7b3838cf417990f369f31346f19d29884e
Sung Jun Hong <sung.j.hong93@gmail.com>
2019-03-01 16:14:58 -0800

    REMOVE: Removed API keys

--- src/utils.js
+++ src/utils.js
@@ -8,7 +8,7 @@ export function load_google_maps() {
 		}
 
 		const script = document.createElement("script");
-		const API_KEY = 'AIzaSyD-dmBFJdqCcuK3-t5VKYtUi19LH8iSV3Q';
+		const API_KEY = "YOUR API KEY";
 		script.async = true;
 		script.defer = true;
 		script.src = `https://maps.googleapis.com/maps/api/js?libraries=places&key=${API_KEY}&callback=resolveGoogleMapsPromise`;
@@ -20,6 +20,6 @@ export function load_google_maps() {
 export function load_places(city, query) {
 	let place = city
 	let interest = query
-	var apiURL = `https://api.foursquare.com/v2/venues/search?client_id=OAJ2HTHSU3WUSS4EXRZYVLZAHF4ALQNKPUCM0MQ0IIZYPEVI&client_secret=SEVVF2CXXOICNGRVQ41FOEYOJVENVSNEGK02YYYY4FMYAYBJ&v=20180323&limit=20&near=${place}&query=${interest}`
+	var apiURL = `https://api.foursquare.com/v2/venues/search?client_id=YOUR_CLIENT_ID&client_secret=YOUR_CLIENT_SECRET&v=20180323&limit=20&near=${place}&query=${interest}`
 	return fetch(apiURL).then((res) => res.json())
 }
